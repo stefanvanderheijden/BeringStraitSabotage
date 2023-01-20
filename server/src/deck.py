8dfcde50aebbdd5359e8959c394838035bfcbce4
Umit Sheombarsing <u.sheombarsing@primevision.com>
2023-01-20 13:27:14 +0100

    removed drawn card function

--- server/src/deck.py
+++ server/src/deck.py
@@ -2,19 +2,21 @@ import random
 
 
 class Deck:
-    def __init__(self, type1, type2, num_cards):
+    def __init__(self, type1, amount_type_1, type2, amount_type_2):
         self.type1 = type1
+        self.amount_type_1 = amount_type_1
         self.type2 = type2
-        self.num_cards = num_cards
+        self.amount_type_2 = amount_type_2
+        self.num_cards = amount_type_1 + amount_type_2
+
         self.cards = []
         self.create_deck()
         self.shuffle()
-        self.drawn_cards = []
 
     def create_deck(self):
-        for i in range(self.num_cards):
+        for i in range(self.amount_type_1):
             self.cards.append(self.type1)
-        for i in range(self.num_cards):
+        for i in range(self.amount_type_2):
             self.cards.append(self.type2)
 
     def shuffle(self):
@@ -23,16 +25,17 @@ class Deck:
     def draw(self):
         if len(self.cards) > 0:
             drawn_card = self.cards.pop()
-            self.drawn_cards.append(drawn_card)
             return drawn_card
         else:
+            print("Deck empty!")
             return None
 
     def add_card(self, card):
         self.cards.append(card)
 
-    def get_num_cards(self):
-        return len(self.cards)
+    def add_deck(self, deck):
+        for card in deck:
+            self.add_card(card)
 
-    def get_drawn_cards(self):
-        return self.drawn_cards
+    def get_deck_height(self):
+        return len(self.cards)
